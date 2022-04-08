31799f57da90624bebf6ad0e844dea9d0c988d51
danamansana <danielappel@nypl.org>
2022-04-08 15:08:42 -0400

    Fix linting

--- app.rb
+++ app.rb
@@ -6,25 +6,26 @@ def init
     $logger.debug 'Initialized'
 end
 
+# rubocop:disable Lint/UnusedMethodArgument
 def handle_event(event:, context:)
     init
-
 
     begin
         parse_dates event
     rescue JSON::ParserError => e
-      create_response(400, { error_message: e.message })
+        create_response(400, { error_message: e.message })
     rescue StandardError => e
         create_response(500, { error_message: e.message })
     end
 end
+# rubocop:enable Lint/UnusedMethodArgument
 
 def parse_dates(event)
-    return create_response(400, { message: 'Request must have body' }) if !event['body']
+    return create_response(400, { message: 'Request must have body' }) unless event['body']
 
     dates = JSON.parse(event['body'])['dates']
-    if !dates || !(dates.is_a? Array) || !(dates.all? {|date| date.is_a? String })
-      return create_response(400, { message: 'Request must have array of dates as strings' })
+    if !dates || !(dates.is_a? Array) || !(dates.all? { |date| date.is_a? String })
+        return create_response(400, { message: 'Request must have array of dates as strings' })
     end
 
     parsed_dates = dates.map { |date| [date, Timetwister.parse(date)] }.to_h
